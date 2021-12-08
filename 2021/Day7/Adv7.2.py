d137e69ccfa8e90c54742c1e85ac7304561a09c5
Magicianic <92609537+Magicianic@users.noreply.github.com>
2021-12-08 00:21:28 +0000

    Update Adv7.2.py

--- 2021/Day7/Adv7.2.py
+++ 2021/Day7/Adv7.2.py
@@ -8,7 +8,7 @@ pos = 0
 for position in range (max(input) + 1) :
     total = 0
     for crab in input :
-        total += sum(range(abs(crab - position) + 1))
+        total += abs(crab - position)*(abs(crab - position) + 1) / 2
     if prevsum is None or total < prevsum :
         prevsum = total
         pos = position
