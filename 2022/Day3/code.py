3287898265f2d435dc71f6d9047b2cfa2ffc58ab
MarcoFreitas <marcojpfreitas@outlook.pt>
2023-11-08 23:47:06 +0000

    Day3 Part2 2022

--- 2022/Day3/code.py
+++ 2022/Day3/code.py
@@ -19,3 +19,18 @@ def get_common(bag):
 priority_sum = sum(list(map(get_common, puzzle_list)))
 
 print(f"Answer 1: The total priority is {priority_sum}")
+
+GROUP_SIZE = 3
+
+def get_common_badges(elves):
+    elves_content = sorted(elves, key=len)
+
+    for letter in elves_content[0]:
+        if letter in elves_content[1] and letter in elves_content[2]:
+            return get_priority(letter)
+
+elves_groups = [puzzle_list[i:i+GROUP_SIZE] for i in range(0,len(puzzle_list), GROUP_SIZE)]
+
+priority_sum_2 = sum(list(map(get_common_badges, elves_groups)))
+
+print(f"Answer 2: The total priority is {priority_sum_2}")
